71b435d2261253b84efb7a6517b8ce3428edaf26
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,15 @@
-output "app_service_source_control_tokens" {
-  description = "All app_service_source_control_token resources"
-  value       = azurerm_app_service_source_control_token.app_service_source_control_tokens
-  sensitive   = true
-}
 output "app_service_source_control_tokens_token" {
-  description = "List of token values across all app_service_source_control_tokens"
-  value       = [for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : v.token]
+  description = "Map of token values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token }
   sensitive   = true
 }
 output "app_service_source_control_tokens_token_secret" {
-  description = "List of token_secret values across all app_service_source_control_tokens"
-  value       = [for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : v.token_secret]
+  description = "Map of token_secret values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token_secret }
   sensitive   = true
 }
 output "app_service_source_control_tokens_type" {
-  description = "List of type values across all app_service_source_control_tokens"
-  value       = [for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : v.type]
+  description = "Map of type values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.type }
 }
 
