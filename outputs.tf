2e13f214bdcb686ed2ed99034e9a8c24281436db
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "app_service_source_control_tokens_id" {
   description = "Map of id values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
-  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.id }
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "app_service_source_control_tokens_token" {
   description = "Map of token values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
-  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token }
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token if v.token != null && length(v.token) > 0 }
   sensitive   = true
 }
 output "app_service_source_control_tokens_token_secret" {
   description = "Map of token_secret values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
-  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token_secret }
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.token_secret if v.token_secret != null && length(v.token_secret) > 0 }
   sensitive   = true
 }
 output "app_service_source_control_tokens_type" {
   description = "Map of type values across all app_service_source_control_tokens, keyed the same as var.app_service_source_control_tokens"
-  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.type }
+  value       = { for k, v in azurerm_app_service_source_control_token.app_service_source_control_tokens : k => v.type if v.type != null && length(v.type) > 0 }
 }
 
